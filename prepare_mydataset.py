427d59aefbe6d581c99a78eb6206dbf4fc1f8fdc
Shimmm <39029429+Shimmm@users.noreply.github.com>
2020-11-08 17:05:29 +0100

    Add files via upload

---
+++ prepare_mydataset.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+import matplotlib.patches as patches
+import os
+
+
+
+
+
+
+# In[6]:
+
+
+path = "300w/"
+path_train_image = "300w/300w_train_images.txt"
+path_train_lm = "300w/300w_train_landmarks.txt"
+path_test_image = "300w/helen_testset.txt"
+path_test_lm = "300w/helen_testset_landmarks.txt"
+
+
+# In[2]:
+
+
+def load_images(path, datalist):
+    """ load image files
+        @path: database path
+        @datalist: the list containing all the images path
+    """
+    dataset = []
+    for line in datalist:
+        im = Image.open(path+line)
+        dataset.append(im)
+    return dataset
+
+def load_landmarks(path, datalist):
+    """ load face landmarks files
+        @path: database path
+        @datalist: the list containing all the landmarks files path
+    """
+    dataset = []
+    for line in datalist:
+        lm = np.loadtxt(path+line)
+        lm = np.array(lm)
+        dataset.append(lm)
+    return np.array(dataset)
+
+
+def load_train():
+    train_ims = []
+    train_lms = []
+    fo = open(path_train_image, "r")
+    for line in fo.readlines(): 
+        line = line.strip()
+        train_ims.append(line)
+
+    fo = open(path_train_lm, "r")
+    for line in fo.readlines(): 
+        line = line.strip()
+        train_lms.append(line)
+    
+    
+    return train_ims, train_lms
+
+def load_test():
+    test_ims = []
+    test_lms = []
+    fo = open(path_test_image, "r")
+    for line in fo.readlines(): 
+        line = line.strip()
+        test_ims.append(line)
+
+
+    fo = open(path_test_lm, "r")
+    for line in fo.readlines(): 
+        line = line.strip()
+        test_lms.append(line)  
+    
+    return test_ims, test_lms
+
+
+
+train_im, train_lm = load_train() 
+test_im, test_lm = load_test()
+
+# print(len(train_im), len(test_im))
+
+
+# In[8]:
+
+
+data_train_ims = load_images(path, train_im)
+data_test_ims = load_images(path, test_im)
+data_train_lms = load_landmarks(path, train_lm)
+data_test_lms = load_landmarks(path, test_lm)
+# print(data_train_ims.shape, data_train_lms.shape)
+
+print("------------------> date loaded successfully!")
+
+def prepare_dataset(save_path, data, lms):
+    print("------------------> preparing my dateset...")
+    
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    for n in range(len(data)):
+        im = data[n]
+        # fig1,ax1 = plt.subplots(1)
+        # ax1.imshow(im)
+               
+        # augmenter les donnees
+        lm = lms[n]
+        # ax1.plot(lm[:,0],lm[:,1],'r.')
+        length = np.max(lm[:,0]) - np.min(lm[:,0])
+        width = np.max(lm[:,1]) - np.min(lm[:,1])
+        
+        length = 1.3 * length
+        width = 1.3 * width
+        
+        left, top = np.min(lm[:,0])- 0.15*length, np.min(lm[:,1])-0.15*width
+        right, bottom = np.min(lm[:,0]) + 0.85*length, np.min(lm[:,1]) + 0.85*width
+        
+        
+        # rect = patches.Rectangle((np.min(lm[:,0])- 0.15*length, np.min(lm[:,1])-0.15*width), length, width, linewidth=2,edgecolor='r',fill=None, alpha=1)
+        # ax1.add_patch(rect)
+        
+        # plt.show()
+        
+        
+        
+        im1 = im.crop((left, top, right, bottom)) 
+        w,h = im1.size
+        # fig2, ax2 = plt.subplots()
+        im1 = im1.resize((128,128), Image.ANTIALIAS)
+        # plt.imshow(im1)
+        
+        im1.save(save_path+str(n)+".jpg")
+        
+        
+        lm[:,0] = lm[:,0] - left
+        lm[:,1] = lm[:,1] - top
+        
+        factor = [128/w, 128/h]
+        lm[:,0] = lm[:,0] * factor[0]
+        lm[:,1] = lm[:,1] * factor[1]
+        
+        np.save(save_path+str(n)+".npy", lm)
+        # ax2.plot(lm[:,0],lm[:,1],'r.')
+    print("------------------> my dateset prepared successfully!")
+ 
+def calculate_moyen(path, lms):
+    n_lms = len(lms)
+    n_pts = len(lms[0])
+    pts = np.zeros([n_lms, n_pts, 2])
+    for n in range(len(lms)):
+        pt = np.load(path+str(n)+".npy")
+        pts[n,:,:] = pt
+        
+    plm = np.mean(pts, axis=0)
+    print("------------------> landmarks mean calculated successfully!")
+    return plm
+
+def perturbations(path, pts_moyens, n):
+    translation = np.round((np.random.rand(2,n)-0.5)*40) # random translation +-20pixels
+    echelle = (np.random.rand(2,n)-0.5) * 0.4 # random factor +- 0.2
+
+    
+    for i in range(n):
+        lm = np.copy(pts_moyens)
+        lm[:,0] = lm[:,0] - translation[0,i]
+        lm[:,1] = lm[:,1] - translation[1,i]
+        
+        lm[:,0] = lm[:,0] * (echelle[0,i] + 1)
+        lm[:,1] = lm[:,1] * (echelle[1,i] + 1)
+        np.save(path+str(i+1)+"_perturbation.npy", lm)
+        
+
+
+def visualise_mydataset(path_train, path_test):
+    pt_moyen_train = np.load(path_train+"train_pts_moyens.npy")
+    pt_moyen_test = np.load(path_test+"test_pts_moyens.npy")
+    
+    fig1,ax1 = plt.subplots()
+    im1 = Image.open(path_test+str(5)+".jpg")
+    ax1.imshow(im1)
+    ax1.plot(pt_moyen_test[:,0], pt_moyen_test[:,1], 'b.')
+    
+    fig2,ax2 = plt.subplots()
+    im2 = Image.open(path_train+str(5)+".jpg")
+    ax2.imshow(im2)
+    ax2.plot(pt_moyen_train[:,0], pt_moyen_train[:,1], 'b.')
+    
+    im = Image.open(path_train+str(10)+".jpg")
+    for i in range(1,11):
+        fig,ax = plt.subplots()
+        ax.imshow(im)
+        pt_moyen_p = np.load(path_train+str(i)+"_perturbation.npy")
+        ax.plot(pt_moyen_p[:,0], pt_moyen_p[:,1], 'r.')
+
+    
+path_train = "mydataset/train/"
+path_test = "mydataset/test/"
+
+# prepare_dataset(path_train, data_train_ims, data_train_lms)
+pt_moyen_train = calculate_moyen(path_train, data_train_lms)
+np.save(path_train+"train_pts_moyens.npy", pt_moyen_train)
+
+# generate perturbations
+perturbations(path_train, pt_moyen_train, 10)
+
+prepare_dataset(path_test, data_test_ims, data_test_lms)
+pt_moyen_test = calculate_moyen(path_test, data_test_lms)
+np.save(path_test+"test_pts_moyens.npy", pt_moyen_test)
+
+# decomment to visualise my dataset
+# visualise_mydataset(path_train, path_test)
+# In[ ]:
+
+
+
+
